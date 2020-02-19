115df0913cc2ab694ac8f7dca01187e2fa2e57ba
Paul M. Christian <paulmchristian@gmail.com>
2020-02-19 17:35:24 -0600

    theremin controls

--- Assets/Scripts/Theremin.cs
+++ Assets/Scripts/Theremin.cs
@@ -9,16 +9,21 @@ public class Theremin : MonoBehaviour
 
     public GameObject ampObj;
     public GameObject freqObj;
-    public GameObject controllerLeft;
-    public GameObject controllerRight;
+    public GameObject pointerLeft;
+    public GameObject pointerRight;
+    private OVRInput.Controller controllerLeft;
+    private OVRInput.Controller controllerRight;
 
     private string _addr = "/theremin";
-    private OscMessage _freq;
-    private OscMessage _amp;
-    private OscMessage _priX;
-    private OscMessage _secX;
-    private OscMessage _priY;
-    private OscMessage _secY;
+    private OscMessage freq;
+    private OscMessage amp;
+    private OscMessage priX;
+    private OscMessage priY;
+    private OscMessage secX;
+    private OscMessage secY;
+    private OscMessage click;
+    private OscMessage noise;
+    private OscMessage voice;
     
     
 	// Use this for initialization
@@ -26,40 +31,48 @@ public class Theremin : MonoBehaviour
 	{
 		_oscOutMax = GameObject.Find("OSC").GetComponents<OscOut>().First(oscOUt => oscOUt.port == 8000);
 
-//        ampObj = GameObject.Find("amp");
-//        freqObj = GameObject.Find("freq");
-//        controllerLeft = GameObject.Find("LeftPointer");
-//        controllerRight = GameObject.Find("RightPointer");
-        
-        _freq = new OscMessage($"{_addr}/freq");
-        _amp = new OscMessage($"{_addr}/amp");
-        _priX = new OscMessage($"{_addr}/pri/x");
-        _priY = new OscMessage($"{_addr}/pri/y");
-        _secX = new OscMessage($"{_addr}/sec/x");
-        _secY = new OscMessage($"{_addr}/sec/y");
+        freq = new OscMessage($"{_addr}/freq");
+        amp = new OscMessage($"{_addr}/amp");
+        priX = new OscMessage($"{_addr}/pri/x");
+        priY = new OscMessage($"{_addr}/pri/y");
+        secX = new OscMessage($"{_addr}/sec/x");
+        secY = new OscMessage($"{_addr}/sec/y");
+        click = new OscMessage($"{_addr}/click");
+        click.Set(0, 1);
+        noise = new OscMessage($"{_addr}/noise");
+        voice = new OscMessage($"{_addr}/voice");
+
+        controllerLeft = pointerLeft.GetComponent<Pointer>().Controller;
+        controllerRight = pointerRight.GetComponent<Pointer>().Controller;
 	}
 
 	float dist(GameObject go1, GameObject go2) =>
 		Vector3.Distance(go1.transform.position, go2.transform.position);
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
-        _freq.Set(0, dist(freqObj, controllerRight));
-        _amp.Set(0, dist(ampObj, controllerLeft)); 
-        
-        var priThumb = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
-        var secThumb = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
-        _priX.Set(0, priThumb.x);
-        _priY.Set(0, priThumb.y);
-        _secX.Set(0, secThumb.x);
-        _secY.Set(0, secThumb.y);
+	    freq.Set(0, dist(freqObj, pointerRight));
+	    amp.Set(0, dist(ampObj, pointerLeft));
 
-        _oscOutMax.Send(_freq);
-        _oscOutMax.Send(_amp);
-        _oscOutMax.Send(_priX);
-        _oscOutMax.Send(_priY);
-        _oscOutMax.Send(_secX);
-        _oscOutMax.Send(_secY);
+	    var priThumb = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, controllerLeft);
+	    var secThumb = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, controllerRight);
+	    priX.Set(0, priThumb.x);
+	    priY.Set(0, priThumb.y);
+	    secX.Set(0, secThumb.x);
+	    secY.Set(0, secThumb.y);
+	    noise.Set(0, OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controllerLeft));
+	    voice.Set(0, OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controllerRight));
+
+	    _oscOutMax.Send(freq);
+	    _oscOutMax.Send(amp);
+	    _oscOutMax.Send(priX);
+	    _oscOutMax.Send(priY);
+	    _oscOutMax.Send(secX);
+	    _oscOutMax.Send(secY);
+	    _oscOutMax.Send(noise);
+	    _oscOutMax.Send(voice);
+
+	    if (OVRInput.GetDown(OVRInput.Button.One, controllerLeft)) _oscOutMax.Send(click);
     }
 }
