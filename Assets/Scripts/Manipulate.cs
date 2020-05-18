3edbf75a15228790f22028bebcaea66d063c7165
Paul M. Christian <paulmchristian@gmail.com>
2020-05-17 20:26:17 -0500

    add uDesktopDuplication; add ability to lock an object to LocalAvatar/base

--- Assets/Scripts/Manipulate.cs
+++ Assets/Scripts/Manipulate.cs
@@ -6,24 +6,29 @@ using UnityEngine;
 
 public class Manipulate : MonoBehaviour
 {
-    public bool IsPinching { get; private set; }
-    public bool IsGrabbing { get; private set; }
-    public bool IsTouching { get; private set; }
+    public bool IsStretching;// { get; private set; }
+    public bool IsGrabbing;// { get; private set; }
+    public bool IsTouching;// { get; private set; }
+
+    public bool ConstrainRoll;
 
     private GameObject _touchControllerGrab;
     private OVRInput.Controller _controllerMaskGrab = OVRInput.Controller.None;
     private Transform _parentAtGrab;
     
     private float _controllerDistanceAtPinchStart;
-    private GameObject _touchControllerPinch;
-    private OVRInput.Controller _controllerMaskPinch = OVRInput.Controller.None;
-    private Vector3 _scaleAtPinchStart;
+    private GameObject _touchControllerStretch;
+    private OVRInput.Controller _controllerMaskStretch = OVRInput.Controller.None;
+    private Vector3 _scaleAtStretchStart;
 
+    private Transform _parentAtStart;
+    
     private Save _save;
 
     void Start()
     {
         _save = GameObject.Find("App").GetComponent<Save>();
+        _parentAtStart = transform.parent;
     }
 
     // Update is called once per frame
@@ -40,58 +45,86 @@ public class Manipulate : MonoBehaviour
             transform.SetParent(_touchControllerGrab.transform, true);
         }
 
+        
         if (IsTouching && OVRInput.GetDown(OVRInput.Button.One, _controllerMaskGrab))
-        {
+        {// Touch and button 1 to save position
             _save.SavePosition(gameObject);
         }
         
-        if (
-            IsGrabbing &&
-            _controllerMaskPinch != OVRInput.Controller.None &&
-            OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, _controllerMaskPinch)
-        )
-        {// Pinch start
-            IsPinching = true;
-            Vibe(0.5f, _controllerMaskGrab);
-            Vibe(0.2f, _controllerMaskPinch);
-            _scaleAtPinchStart = transform.localScale;
-            _controllerDistanceAtPinchStart = (_touchControllerGrab.transform.position - _touchControllerPinch.transform.position).magnitude;
-        }
+       
 
-        if (IsPinching)
-        {// is Pinching
-            var mag = (_touchControllerGrab.transform.position - _touchControllerPinch.transform.position)
+        if (IsStretching)
+        {// is Stretching
+            
+            var mag = (_touchControllerGrab.transform.position - _touchControllerStretch.transform.position)
                 .magnitude / _controllerDistanceAtPinchStart;
-            transform.localScale = _scaleAtPinchStart * mag;
-        }
-
-        if (IsPinching && (
-                OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, _controllerMaskPinch)
-                || OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, _controllerMaskGrab)
-            ))
-        {// Pinch end
-            _controllerMaskPinch = OVRInput.Controller.None;
-            IsPinching = false;
+            transform.localScale = _scaleAtStretchStart * mag;
+            
+            Vibe(0.2f, _controllerMaskGrab);
+            Vibe(0.2f, _controllerMaskStretch);
+            
+            if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, _controllerMaskStretch)
+                || OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, _controllerMaskGrab))
+            {// Stretch end
+                _controllerMaskStretch = OVRInput.Controller.None;
+                IsStretching = false;
+            }
         }
         
         if (IsGrabbing)
         {// is Grabbing
+            if (ConstrainRoll)
+            {// lock Z-axis
+                transform.rotation = Quaternion.LookRotation(_touchControllerGrab.transform.forward, Vector3.up);
+            }
             
+            if (
+                _controllerMaskStretch != OVRInput.Controller.None &&
+                OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, _controllerMaskStretch)
+            )
+            {// Stretch start
+                IsStretching = true;
+                Vibe(0.5f, _controllerMaskGrab);
+                Vibe(0.5f, _controllerMaskStretch);
+                _scaleAtStretchStart = transform.localScale;
+                _controllerDistanceAtPinchStart = (_touchControllerGrab.transform.position - _touchControllerStretch.transform.position).magnitude;
+            }
+            
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, _controllerMaskGrab))
+            {// Grab and index trigger to possess/dispossess from LocalAvatar/base
+                if (_parentAtGrab == _parentAtStart)
+                {
+                    transform.SetParent(GameObject.Find("LocalAvatar/base").transform, true);
+                }
+                else
+                {
+                    transform.SetParent(_parentAtStart, true);
+                }
+                
+                // TODO: how much of this is necessary? Encapsulate?
+                IsGrabbing = false;
+                IsStretching = false;
+                IsTouching = false;
+                _touchControllerGrab = null;
+                _controllerMaskGrab = OVRInput.Controller.None;
+                _controllerMaskStretch = OVRInput.Controller.None;
+                _parentAtGrab = null;
+            }
+            
+            if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, _controllerMaskGrab))
+            {// Grab end
+                transform.SetParent(_parentAtGrab, true);
+                IsGrabbing = false;
+                IsStretching = false;
+                IsTouching = false;
+            }
         }
-
-        if (IsGrabbing && OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, _controllerMaskGrab))
-        {// Grab end
-            transform.SetParent(_parentAtGrab, true);
-            IsGrabbing = false;
-        }
-
-        
     }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         var pointer = other.GetComponent<Pointer>();
-        if (!IsTouching && !IsGrabbing && pointer != null)
+        if (!IsTouching && !IsStretching && !IsGrabbing && pointer != null)
         { // Touch start; Grab is possible
             IsTouching = true;
             _touchControllerGrab = other.gameObject;
@@ -99,11 +132,11 @@ public class Manipulate : MonoBehaviour
             Vibe(0.2f, _controllerMaskGrab);
         } 
         
-        if (IsGrabbing && pointer != null)
-        { // Pinch is possible
-            _controllerMaskPinch = pointer.Controller;
-            _touchControllerPinch = other.gameObject;
-            Vibe(0.2f, _controllerMaskPinch);
+        if (IsGrabbing && !IsStretching && pointer != null)
+        { // Stretch is possible
+            _controllerMaskStretch = pointer.Controller;
+            _touchControllerStretch = other.gameObject;
+            Vibe(0.2f, _controllerMaskStretch);
         }
     }
 
